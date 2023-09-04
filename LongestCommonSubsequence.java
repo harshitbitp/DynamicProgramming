1aaa7b098a563a65b25456a1d6b3ba01c1658342
unknown <harshiit.anand@gmail.com>
2023-09-04 19:30:04 +0530

    Correction in LCS

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -57,7 +57,7 @@ public class LongestCommonSubsequence {
 		for (int i = 0; i < m + 1; i++) {
 			for (int j = 0; j < n + 1; j++) {
 				if (i == 0 || j == 0) {
-					t2[i][j] = 0;
+					t[i][j] = 0;
 				}
 			}
 		}
